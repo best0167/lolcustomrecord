639c1381612d80a49cb82c2659fa22fb897199e4
frame <best0167@naver.com>
2021-07-13 21:52:25 +0900

    오늘의 작업

---
+++ src/main/java/com/best0167/lol/lolcustomrecord/LolcustomrecordApplication.java
@@ -0,0 +1,14 @@
+package com.best0167.lol.lolcustomrecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolcustomrecordApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(LolcustomrecordApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/best0167/lol/lolcustomrecord/LolcustomrecordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.best0167.lol.lolcustomrecord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LolcustomrecordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
